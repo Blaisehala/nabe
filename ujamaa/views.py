cf6e73e435254e6754f1e55f84a817b24b6a85f1
Blaise Hala <blaisehala@gmail.com>
2022-06-19 22:31:13 +0300

    adds view functions

--- ujamaa/views.py
+++ ujamaa/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+posts =[
+  {
+    'Neighbourhood': 'Brownsville',
+    'title': 'Neighbourhood',
+    'content':'Brave Neighbourhood',
+    'date_posted':"August 12 2022"
+  },
+
+   {
+    'Neighbourhood': 'Brownsville',
+    'title': 'Neighbourhood',
+    'content':'Brave Neighbourhood',
+    'date_posted':"August 12 2022"
+  }
+]
+
+
+
+
+
+
+
 
 # Create your views here.
+def home(request):
+  context ={
+    'posts':posts
+  }
+
+  return render(request, 'users/home.html' )
+
+
+def projects(request):
+  context ={
+
+    'posts': posts
+  }
+
+
+  return render(request, 'users/projects.html',context)
+
+  
+
+def about(request):
+
+
+  return render(request, 'users/about.html', {'title': 'About'})
+
