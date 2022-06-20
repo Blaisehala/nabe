9ebdd2b2494e9cbb8de0f78155a5d033e469a669
Blaise Hala <blaisehala@gmail.com>
2022-06-20 05:52:40 +0300

    build:adds urlpattern

--- ujamaa/urls.py
+++ ujamaa/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.home, name='home'),
+    path('', views.home, name='ujamaa-home'),
     path('projects/', views.projects, name='nb-project'),
     path('about/', views.about, name='nb-about'),
 
