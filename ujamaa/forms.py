b12cba484d6f0be7c821f1c4479b3a0d78c8aecf
Blaise Hala <blaisehala@gmail.com>
2022-06-22 03:09:48 +0300

    creates class forms

--- ujamaa/forms.py
+++ ujamaa/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile, Neighbourhood, Business
+from .models import Profile, Neighbourhood, Business,Post
 
 
 
@@ -27,4 +27,26 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
   class Meta:
     model = Profile
-    fields = ['image']
+    exclude = ['user']
+
+
+
+class NewProjectForm(forms.ModelForm):
+  class Meta:
+    model = Neighbourhood
+
+    fields =['title', 'description', 'location','county','neighbourhood_image','neighbourhood_admin','health_department','police_department']
+
+
+class NewBusinessForm(forms.ModelForm):
+  class Meta:
+    model = Business
+
+    fields =['name','description','business_email','business_type']
+    
+
+class NewPostForm(forms.ModelForm):
+  class Meta:
+    model = Post
+
+    fields =['title','image','content','neighbourhood']
