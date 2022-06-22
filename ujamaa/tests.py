e8897aa1188e976abfbcef128799713070e272af
Blaisehala <73476838+Blaisehala@users.noreply.github.com>
2022-06-22 23:51:57 +0300

    Update tests.py

--- ujamaa/tests.py
+++ ujamaa/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+  def setUp(self):
+    self.jiji = Profile(image='http//')
+  def test_instance(self):
+    self.assertTrue(isinstance(self.jiji,Profile))
+    
+    
+ class NeighbourhoodTestClass(TestCase):
+  def setUp(self):
+    self.hood = Neighbourhood (neighbourhood_image='http',title='hey', description='hello',)
+  def test_instance(self):
+    self.assertTrue(isinstance(self.hood,Project))
+  
+  
